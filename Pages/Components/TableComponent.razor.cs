c8eb574186afa617ab28bdb4d41703dafb449f89
LucasN177 <lucas.nagelsmann@gmail.com>
2026-02-26 15:06:32 +0100

    Added click functiionality to table component

--- Pages/Components/TableComponent.razor.cs
+++ Pages/Components/TableComponent.razor.cs
@@ -1,17 +1,24 @@
 using FootballTracker.Core.Models;
 using FootballTracker.Services;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace FootballTracker.Pages.Components;
 
 public partial class TableComponent : ComponentBase
 {
-    
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
     private List<Team> _teams = new();
 
     protected override async Task OnInitializedAsync()
     {
         _teams = await TabellenService.GetBundesligaTabelle(AppStateService.SelectedSeason.Year);
         _teams = _teams.Where(x => x.TabellenPlatz < 6).ToList();
+    }
+    
+    private void NavigateToDetailsPage(TableRowClickEventArgs<Team> args)
+    {
+        if(args.Item != null)
+            NavigationManager.NavigateTo($"TeamDetailsPage/{args.Item.Id}");
     }
 }
